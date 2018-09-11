41b27eb36c165324e4494ae3c3007170e777810e
ZXW <xzhang305@gmail.com>
2018-09-11 19:39:48 -0400

    finish isLeapYear class

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,7 +2,14 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
-
+    
+    public static boolean isLeapYear(int year){
+        if (year % 400 == 0 | (year % 4 == 0 & year % 100 != 0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
